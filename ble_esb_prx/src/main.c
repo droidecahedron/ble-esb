f29a7631e72b6ef948caa39cbf828ad6cc8f238e
droidecahedron <droidecahedron@gmail.com>
2025-06-30 15:49:31 -0700

    initial

---
+++ ble_esb_prx/src/main.c
@@ -0,0 +1,74 @@
+/*
+ * Copyright (c) 2019 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+/*
+MODIFIED SAMPLE TO INCLUDE EXTENSIONS ++
+*/
+
+#include <zephyr/kernel.h>
+#include <zephyr/console/console.h>
+#include <string.h>
+#include <zephyr/sys/printk.h>
+#include <zephyr/types.h>
+#include <zephyr/logging/log.h>
+
+#include <zephyr/drivers/gpio.h> 
+
+#include "app_bt_lbs.h"
+
+#include "app_esb.h"
+
+LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
+
+void on_esb_callback(app_esb_event_t *event)
+{
+	static uint32_t last_counter = 0;
+	static uint32_t counter;
+	switch(event->evt_type) {
+		case APP_ESB_EVT_TX_SUCCESS:
+			LOG_INF("ESB TX success");
+			break;
+		case APP_ESB_EVT_TX_FAIL:
+			LOG_INF("ESB TX failed");
+			break;
+		case APP_ESB_EVT_RX:
+			memcpy((uint8_t*)&counter, event->buf, sizeof(counter));
+			if(counter != (last_counter + 1)) {
+				LOG_WRN("Packet content error! Counter: %i, last counter %i", counter, last_counter);
+			}
+			LOG_INF("ESB RX: 0x%.2X-0x%.2X-0x%.2X-0x%.2X", event->buf[0], event->buf[1], event->buf[2], event->buf[3]);
+			last_counter = counter;
+			break;
+		default:
+			LOG_ERR("Unknown APP ESB event!");
+			break;
+	}
+}
+
+int main(void)
+{
+	int err;
+
+	LOG_INF("ESB PRX BLE Multiprotocol Example");
+	
+	err = app_bt_init();
+	if (err) {
+		LOG_ERR("app_bt init failed (err %d)", err);
+		return err;
+	}
+
+	err = app_esb_init(APP_ESB_MODE_PRX, on_esb_callback);
+	if (err) {
+		LOG_ERR("app_esb init failed (err %d)", err);
+		return err;
+	}
+
+	while (1) {
+		k_sleep(K_MSEC(2000));
+	}
+
+	return 0;
+}
---
+++ ble_esb_ptx/src/main.c
@@ -0,0 +1,80 @@
+/*
+ * Copyright (c) 2019 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+/*
+MODIFIED SAMPLE TO INCLUDE EXTENSIONS ++
+*/
+
+#include <zephyr/kernel.h>
+#include <zephyr/console/console.h>
+#include <string.h>
+#include <zephyr/sys/printk.h>
+#include <zephyr/types.h>
+#include <zephyr/logging/log.h>
+
+#include <zephyr/drivers/gpio.h> 
+
+#include <dk_buttons_and_leds.h>
+
+#include "app_bt_lbs.h"
+
+#include "app_esb.h"
+
+LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
+
+void on_esb_callback(app_esb_event_t *event)
+{
+	switch(event->evt_type) {
+		case APP_ESB_EVT_TX_SUCCESS:
+			LOG_INF("ESB TX success");
+			break;
+		case APP_ESB_EVT_TX_FAIL:
+			LOG_INF("ESB TX failed");
+			break;
+		case APP_ESB_EVT_RX:
+			LOG_INF("ESB RX: 0x%.2x-0x%.2x-0x%.2x-0x%.2x", event->buf[0], event->buf[1], event->buf[2], event->buf[3]);
+			break;
+		default:
+			LOG_ERR("Unknown APP ESB event!");
+			break;
+	}
+}
+
+int main(void)
+{
+	int err;
+
+	LOG_INF("ESB BLE Multiprotocol Example");
+
+	err = app_bt_init();
+	if (err) {
+		LOG_ERR("app_bt init failed (err %d)", err);
+		return err;
+	}
+
+	err = app_esb_init(APP_ESB_MODE_PTX, on_esb_callback);
+	if (err) {
+		LOG_ERR("app_esb init failed (err %d)", err);
+		return err;
+	}
+
+	static app_esb_data_t my_data;
+	my_data.len = 8;
+	int tx_counter = 0;
+	while (1) {
+		memcpy(my_data.data, (uint8_t*)&tx_counter, 4);
+		err = app_esb_send(&my_data);
+		if (err < 0) {
+			LOG_INF("ESB TX upload failed (err %i)", err);
+		}
+		else {
+			LOG_INF("ESB TX upload %.2x-%.2x", (tx_counter& 0xFF), ((tx_counter >> 8) & 0xFF));
+			tx_counter++;
+		}
+		k_sleep(K_MSEC(1500));
+	}
+	return 0;
+}
